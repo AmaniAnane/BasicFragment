5fecdda40e59cc0606beae3fe952f7b83d45be08
yasminch <jessymina.jc@gmail.com>
2019-11-11 11:27:30 +0100

    Initial commit

---
+++ app/src/main/java/com/oumaimasouissi/basicfragment/DynamicFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.oumaimasouissi.basicfragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DynamicFragmentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);setContentView(R.layout.activity_dynamic_fragment);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new MyDynamicFragment()).commit();
+    }
+}
